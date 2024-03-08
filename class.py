760b2148c52ecd1537b0d77d186d499424008fdd
BahareTabibi <vernal.tb@gmail.com>
2024-03-08 19:14:39 +0330

    add student class

--- class.py
+++ class.py
@@ -1,3 +1,6 @@
+from db_con import *
+
+
 class Class:
     def __init__(self, database):
         self.database = database
@@ -17,6 +20,9 @@ class Class:
         self.database.commit()
 
     def update_class(self, id, college=None, seats_num=None, is_smart=None):
+        college = college or self.college
+        seats_num = seats_num or self.seats_num
+        is_smart = is_smart or self.is_smart
         query = "UPDATE classes SET college = %s, seats_num = %s, is_smart = %s WHERE id = %s"
         params = (college, seats_num, is_smart, id)
         self.database.execute_query(query, params)
---
+++ student.py
@@ -0,0 +1,34 @@
+from db_con import *
+
+
+class Student:
+    def __init__(self, database):
+        self.database = database
+
+    def add_student(self, name, f_name, age, college, entrance_year):
+        self.name = name
+        self.f_name = f_name
+        self.age = age
+        self.college = college
+        self.entrance_year = entrance_year
+        query = "INSERT INTO students (name, f_name, age, college, entrance_year) VALUES (%s, %s, %s, %s, %s)"
+        params = (self.name, self.f_name, self.age, self.college, self.entrance_year)
+        self.database.execute_query(query, params)
+        self.database.commit()
+
+    def update_student(self, id, name=None, f_name=None, age=None, college=None, entrance_year=None):
+        name = name or self.name
+        f_name = f_name or self.f_name
+        age = age or self.age
+        college = college or self.college
+        entrance_year = entrance_year or self.entrance_year
+        query = "UPDATE students SET name = %s, f_name = %s, age = %s, college = %s, entrance_year = %s WHERE id = %s"
+        params = (name, f_name, age, college, entrance_year, id)
+        self.database.execute_query(query, params)
+        self.database.commit()
+
+    def delete_student(self, id):
+        query = "DELETE FROM students WHERE id = %s"
+        params = (id,)
+        self.database.execute_query(query, params)
+        self.database.commit()

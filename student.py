a57c6f154d0346aafef46539de578c7f8b56cfa5
saharsasanifar <sasanifarsahar@gmail.com>
2024-03-10 10:01:46 +0330

    file csv but not complete

--- student.py
+++ student.py
@@ -1,10 +1,20 @@
 from db_con import *
-
-
+import re 
+import pandas as pd
+#reminder =======> write email for it
 class Student:
 
     id_ = 1234
-    def add_student(self, name, f_name, age, college, entrance_year):
+    def add_student_with_csv(self,url):
+        self.url = url
+        self.df = pd.read_csv(url)
+        self.name = df["name"]
+        self.f_name = df["f_name"]
+        self.age = df["age"]
+        self.college = df["college"]
+        self.entrance_year = df["entrance_year"]
+
+    def add_student(self, name, f_name, age, college, entrance_year,email):
         super().add_member(name)
         self.f_name = f_name
         self.age = age
@@ -12,17 +22,19 @@ class Student:
         self.entrance_year = entrance_year
         self.id_ = "S" + str(Student.id_)
         Student.id_ += 1
-        query = "INSERT INTO students (name, f_name, age, college, entrance_year, id_) VALUES (%s, %s, %s, %s, %s, %s)"
-        params = (self.name, self.f_name, self.age, self.college, self.entrance_year, self.id_)
+        self.email = email
+        query = "INSERT INTO students (name, f_name, age, college, entrance_year, id_, email) VALUES (%s, %s, %s, %s, %s, %s,%s)"
+        params = (self.name, self.f_name, self.age, self.college, self.entrance_year, self.id_, email)
         self.database.execute_query(query, params)
         self.database.commit()
 
-    def update_student(self, name=None, f_name=None, age=None, college=None, entrance_year=None):
+    def update_student(self, name=None, f_name=None, age=None, college=None, entrance_year=None, email=None):
         super().update_member(name)
         f_name = f_name or self.f_name
         age = age or self.age
         college = college or self.college
         entrance_year = entrance_year or self.entrance_year
+        email = email or self.email
         query = "UPDATE students SET name = %s, f_name = %s, age = %s, college = %s, entrance_year = %s"
         params = (name, f_name, age, college, entrance_year)
         self.database.execute_query(query, params)
